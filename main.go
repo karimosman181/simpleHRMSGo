437dbde836a008facf15c25d0ab7a9104cde6551
Karim Osman <kareemosman199@gmail.com>
2023-07-22 22:20:41 +0300

    add get all employees func

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -16,10 +18,10 @@ type MongoInstance struct {
 }
 
 type Employee struct {
-	ID     string `json:"id"`
+	ID     string `json:"id, omitempty" bson:"_id, omitempty"`
 	Name   string `json:"name"`
-	Salary string `json:salary`
-	Age    string `age`
+	Salary string `json:"salary"`
+	Age    string `json:"age"`
 }
 
 var mg MongoInstance
@@ -27,6 +29,10 @@ var mg MongoInstance
 const dbName = "fiber-hrms"
 const mongoURI = "mongodb://localhost:27017" + dbName
 
+/**
+ *
+ * db connection func
+ **/
 func Connect() error {
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 
@@ -49,6 +55,56 @@ func Connect() error {
 	return nil
 }
 
+/**
+ *
+ * get all employess records
+ **/
+func getEmployees(c *fiber.Ctx) error {
+
+	query := bson.D{{}}
+	cursor, err := mg.Db.Collection("Employees").Find(c.Context(), query)
+	if err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	var employees []Employee = make([]Employee, 0)
+
+	if err := cursor.All(c.Context(), &employees); err != nil {
+		return c.Status(500).SendString(err.Error())
+	}
+
+	return c.JSON(employees)
+
+}
+
+/**
+ *
+ *
+ **/
+func getEmployeeByID(c *fiber.Ctx) error {}
+
+/**
+ *
+ *
+ **/
+func createNewEmployee(c *fiber.Ctx) error {}
+
+/**
+ *
+ *
+ **/
+func updateEmployee(c *fiber.Ctx) error {}
+
+/**
+ *
+ *
+ **/
+func deleteEmploye(c *fiber.Ctx) error {}
+
+/**
+ *
+ *
+ **/
 func main() {
 
 	if err := Connect(); err != nil {
@@ -57,9 +113,9 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/employee", getEmployees())
-	app.Get("/employee/:id", getEmployeeByID())
-	app.Post("/employee", createNewEmployee())
-	app.Put("/employee/:id", updateEmployee())
-	app.Delete("/employee/:id", deleteEmploye())
+	app.Get("/employee", getEmployees)
+	app.Get("/employee/:id", getEmployeeByID)
+	app.Post("/employee", createNewEmployee)
+	app.Put("/employee/:id", updateEmployee)
+	app.Delete("/employee/:id", deleteEmploye)
 }
