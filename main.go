c9b445f6528cd8cfd713de6ac3902107812f301a
Karim Osman <kareemosman199@gmail.com>
2023-07-22 22:00:45 +0300

    add connection to mongo db

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoInstance struct {
@@ -9,4 +15,51 @@ type MongoInstance struct {
 	Db     *mongo.Database
 }
 
-func main() {}
+type Employee struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Salary string `json:salary`
+	Age    string `age`
+}
+
+var mg MongoInstance
+
+const dbName = "fiber-hrms"
+const mongoURI = "mongodb://localhost:27017" + dbName
+
+func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+
+	//define timeOut (so you donot block the entire program)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	db := client.Database(dbName)
+
+	if err != nil {
+		return err
+	}
+
+	mg = MongoInstance{
+		Client: client,
+		Db:     db,
+	}
+
+	return nil
+}
+
+func main() {
+
+	if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	app := fiber.New()
+
+	app.Get("/employee", getEmployees())
+	app.Get("/employee/:id", getEmployeeByID())
+	app.Post("/employee", createNewEmployee())
+	app.Put("/employee/:id", updateEmployee())
+	app.Delete("/employee/:id", deleteEmploye())
+}
